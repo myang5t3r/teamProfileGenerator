5248c2a849a44384587a210009d2c1d30ce30740
myang5t3r <matthew.yang@hunterdouglas.com>
2022-08-28 09:26:27 -0600

    Added and tested questions for manager, rest of team will follow the object format

--- index.js
+++ index.js
@@ -6,9 +6,35 @@ const jest = require('jest');
 const fs = require('fs')
 
 // import classes that the employee object so we can pass it to html creator
-const Employee = require('./lib/employee.js');
-const Manager = require('./lib/manager.js');
-const Engineer = require('./lib/engineer.js');
-const Intern = require('./lib/intern.js');
+// const Employee = require('./lib/employee.js');
+// const Manager = require('./lib/manager.js');
+// const Engineer = require('./lib/engineer.js');
+// const Intern = require('./lib/intern.js');
 
-// 
+// Create questions for each type of employee to be called during prompt
+const employeeRoles = {
+    manager: [{    
+        type: 'input',
+        name: 'managerName',
+        message: 'What is your name?',
+    },
+    {
+        type: 'input',
+        name: 'employeeId',
+        message: 'Please enter your employeeId',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email',
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Please enter your office number'
+    }
+    ]
+}
+
+inquirer.prompt(employeeRoles.manager)
+    .then(answers => console.log(answers));
